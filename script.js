53032915f0392efb6af36a380112ad186db550c7
Caroline Winkel <carolinewinkel@Carolines-MacBook-Pro.local>
2019-03-28 10:18:49 +0100

    Instargam

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function useinstadata(data) {
-console.log(data);  
+function useinstadata(instas) {
+  let pe = document.querySelector("#instas");
+  instas.data.forEach(item => {
+    let el = document.createElement("img");
+    el.setAttribute("src", item.images.standard_resolution.url);
+    el.setAttribute("width", item.images.standard_resolution.width);
+    el.setAttribute("height", item.images.standard_resolution.height);
+    el.setAttribute("alt", item.caption.text);
+    pe.append(el);
+  });
 }
 
 
 window.addEventListener("load", function () {
   fetch("https://api.instagram.com/v1/users/self/media/recent/?access_token=11928456755.5f06755.01a481de03cf4ff98edf462b7aef617e")
     .then(res => res.json())
-    .then(useinstadata);
+    .then(data => {
+      document.querySelector("#butikken").remove();
+      useinstadata(data);
+    });
 });
